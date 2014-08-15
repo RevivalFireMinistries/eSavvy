31f69d68c12771597639c89348b4a75671cb42e1
Russel Mupfumira <russel@rfm.org.za>
2014-08-15 17:49:15 +0200

    fixed file name problem with git

---
+++ src/main/java/za/org/rfm/beans/BulletinScroller.java
@@ -0,0 +1,11 @@
+package za.org.rfm.beans;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Russel
+ * Date: 8/15/14
+ * Time: 5:48 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class BulletinScroller {
+}
